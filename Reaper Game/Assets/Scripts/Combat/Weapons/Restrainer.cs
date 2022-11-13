240ecbfe7b649b6157a08bfa814321c3e28d5d29
Slaith12 <34562469+Slaith12@users.noreply.github.com>
2022-11-12 21:34:25 -0800

    Added projectile functionality
    Also updated tags and water bottle sprite to help

--- Reaper Game/Assets/Scripts/Combat/Weapons/Restrainer.cs
+++ Reaper Game/Assets/Scripts/Combat/Weapons/Restrainer.cs
@@ -7,7 +7,21 @@ namespace Reaper.Combat
     [CreateAssetMenu(menuName = "Weapons/Restrainer")]
     public class Restrainer : Weapon
     {
+        //I'll likely create a ranged weapon class that restrainer derives from and has these variables
+        [SerializeField] protected Sprite projectileSprite;
+        [SerializeField] protected float fireCooldown = 0.5f;
+        [SerializeField] protected float shotSpeed = 2;
+        [SerializeField] protected Vector2 shotSize = Vector2.one;
+        [SerializeField] protected float shotDuration = 3;
+        [SerializeField] protected List<string> targets;
+
         public override bool primaryHasHold => true;
         public override bool secondaryHasHold => false;
+        public override void SecondaryFireDown(WeaponUser user, Vector2 facing)
+        {
+            Projectile projectile = Projectile.Create(projectileSprite, targets, Projectile.defaultEnviroTargets, facing * shotSpeed, shotDuration);
+            projectile.transform.SetTransform(user.transform.position, facing.ToAngle(), shotSize);
+            user.SetCooldown(fireCooldown);
+        }
     }
 }
